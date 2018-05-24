9577f96d580fdf6577f4af8555e8cf15a79feff2
Jun Okumura <okumura@haw.co.jp>
2018-05-24 11:39:48 +0900

    feat: add directory clean logic for export and import test cases.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,6 +34,17 @@ RSpec.configure do |config|
     end
   end
 
+  config.before(:suite) do
+    neo4j_session
+    unless Dir.glob(File.join(neo4j_dir, "*")).empty?
+      STDERR.write "file exists in #{neo4j_dir}.\nmove them to #{neo4j_dir.gsub("import", "backup")}.\nplease move #{neo4j_dir.gsub("import", "backup")} to your directory.\n"
+      FileUtils.mv(neo4j_dir, neo4j_dir.gsub("import", "backup"), :secure => true)
+    else
+      FileUtils.rm_rf(neo4j_dir, :secure => true)
+    end
+    FileUtils.mkdir(neo4j_dir)
+  end
+
   config.after(:each) do |example|
     if example.metadata[:cli]
       Dir.glob(["*.pid", "*.log"]).each do |f|
@@ -42,6 +53,12 @@ RSpec.configure do |config|
     else
       Neo4j::ActiveBase.current_session.query('MATCH(n) DETACH DELETE n')
     end
+  end
+
+  config.after(:suite) do
+    neo4j_session
+    FileUtils.rm_rf(neo4j_dir, :secure => true)
+    FileUtils.mkdir(neo4j_dir)
   end
 
   def test_configuration
@@ -53,5 +70,9 @@ RSpec.configure do |config|
     config = config[:blockgraph]
     neo4j_adaptor = Neo4j::Core::CypherSession::Adaptors::HTTP.new(config[:neo4j][:server], {basic_auth: config[:neo4j][:basic_auth], initialize: {request: {timeout: 600, open_timeout: 2}}})
     Neo4j::ActiveBase.on_establish_session { Neo4j::Core::CypherSession.new(neo4j_adaptor) }
+  end
+
+  def neo4j_dir
+    Neo4j::ActiveBase.current_session.query('CALL dbms.listConfig() yield name,value WHERE name=~"dbms.directories.import" RETURN value').rows[0].first
   end
 end
